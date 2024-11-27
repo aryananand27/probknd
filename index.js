dcb372dbbfa062520755ca6a0ffc58159f175271
aryananand27 <mgsaryananand2003@example.com>
2024-11-27 23:44:23 +0530

    first commit

--- index.js
+++ index.js
@@ -269,6 +269,13 @@ app.post("/verify-payment", (req, res) => {
     }
   });
 
+  app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
+
 app.listen(8000,(err)=>{
     if(err){
         console.log(err);
